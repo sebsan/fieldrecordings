820749d1802cac2f381e50dac0872b76bf439b57
Sebastien Sanfilippo <sebastien@sebastien-MacBookPro.(none)>
2011-07-14 14:15:51 +0200

    facto style menu

--- menu.php
+++ menu.php
@@ -64,35 +64,35 @@ var theCity = "<?php echo $_GET["city"] ?>";
 			<div class="menu_category">
 			Brussels
 			</div>
-			<a href="#">Berlusconi Claims He Won't Seek Re-Election</a>
-			<a href="#">British Gas Announces Energy Price Hike</a>
-			<a href="#">Humanitarian Emergency In East Africa</a>
-			<a href="#">DEC Launches Appeal As 10m Face Starvation</a>
-			<a href="#">Clegg: No Triumphalist Euro Schadenfreude</a>
-			<a href="#">Kate Makes Girl's Princess Dream Come True</a>
-			<a href="#">From 6am, New Olympic Tickets On Sale</a>
-			<a href="#">They've Come To Say Goodbye To Their Childhoods</a>
+			<a class="menu_base" href="#">Berlusconi Claims He Won't Seek Re-Election</a>
+			<a class="menu_base" href="#">British Gas Announces Energy Price Hike</a>
+			<a class="menu_base" href="#">Humanitarian Emergency In East Africa</a>
+			<a class="menu_base" href="#">DEC Launches Appeal As 10m Face Starvation</a>
+			<a class="menu_base" href="#">Clegg: No Triumphalist Euro Schadenfreude</a>
+			<a class="menu_base" href="#">Kate Makes Girl's Princess Dream Come True</a>
+			<a class="menu_base" href="#">From 6am, New Olympic Tickets On Sale</a>
+			<a class="menu_base" href="#">They've Come To Say Goodbye To Their Childhoods</a>
 		</div>
 	</span>
 	<span>
 		<div id="colonne_2" class="index_col">
-			<a href="#">Seven People Killed In Michigan Shooting Spree</a>
-			<a href="#">Oxbridge: The Entry Gap Highlighted</a>
-			<a href="#">Decision On BSkyB Takeover Could Take Weeks After Surge In Online Campaigning</a>
-			<a href="#">Police Prepare To Rally Against Cuts As Pay Freeze Looms</a>
-			<a href="#">Six-Figure Sum Paid To Met, Claims Evening Standard</a>
-			<a href="#">Alarm Over Aircraft Carrier Programme</a>
-			<a href="#">Arianna's Big Day In London</a>
-			<a href="#">David Laws' Swift Return To Parliament</a>
+			<a class="menu_base" href="#">Seven People Killed In Michigan Shooting Spree</a>
+			<a class="menu_base" href="#">Oxbridge: The Entry Gap Highlighted</a>
+			<a class="menu_base" href="#">Decision On BSkyB Takeover Could Take Weeks After Surge In Online Campaigning</a>
+			<a class="menu_base" href="#">Police Prepare To Rally Against Cuts As Pay Freeze Looms</a>
+			<a class="menu_base" href="#">Six-Figure Sum Paid To Met, Claims Evening Standard</a>
+			<a class="menu_base" href="#">Alarm Over Aircraft Carrier Programme</a>
+			<a class="menu_base" href="#">Arianna's Big Day In London</a>
+			<a class="menu_base" href="#">David Laws' Swift Return To Parliament</a>
 		</div>
 	</span>
 	<span>
 		<div id="colonne_3" class="index_col">
-			<a href="#">News Of The World Editor Arrested As Phone-Hacking Scandal Deepens</a>
-			<a href="#">The Newspaper's Closing In Pictures</a>
-			<a href="#">Boehner Backs Obama's Call For Far-Reaching Debt Ceiling Plan</a>
-			<a href="#">Space Shuttle Prepares For Final Flight</a>
-			<a href="#">White House Rules Out Constitutional Option On Debt Ceiling</a>
+			<a class="menu_base" href="#">News Of The World Editor Arrested As Phone-Hacking Scandal Deepens</a>
+			<a class="menu_base" href="#">The Newspaper's Closing In Pictures</a>
+			<a class="menu_base" href="#">Boehner Backs Obama's Call For Far-Reaching Debt Ceiling Plan</a>
+			<a class="menu_base" href="#">Space Shuttle Prepares For Final Flight</a>
+			<a class="menu_base" href="#">White House Rules Out Constitutional Option On Debt Ceiling</a>
 		</div>
 	</span>
 	<span>
@@ -100,31 +100,31 @@ var theCity = "<?php echo $_GET["city"] ?>";
 			<div class="menu_category">
 			Madrid
 			</div>
-			<a href="#">A Complaint From Palin In Newly-Released Email</a>
-			<a href="#">New Twist In Casey Anthony Release Date</a>
-			<a href="#">Guess Which Bank May Soon Become The Biggest In The U.S.</a>
-			<a href="#">House Rejects Effort To Prohibit Funds For U.S. Involvement In Libya</a>
-			<a href="#">Michael Bloomberg To Officiate Gay Wedding</a>
-			<a href="#">Obama To Unveil Gun Control Reforms</a>
-			<a href="#">Federal Prosecutors Take Softer Approach To Corporate Crime</a>
-			<a href="#">Montana Risks Losing Federal Education Funding</a>
+			<a class="menu_base" href="#">A Complaint From Palin In Newly-Released Email</a>
+			<a class="menu_base" href="#">New Twist In Casey Anthony Release Date</a>
+			<a class="menu_base" href="#">Guess Which Bank May Soon Become The Biggest In The U.S.</a>
+			<a class="menu_base" href="#">House Rejects Effort To Prohibit Funds For U.S. Involvement In Libya</a>
+			<a class="menu_base" href="#">Michael Bloomberg To Officiate Gay Wedding</a>
+			<a class="menu_base" href="#">Obama To Unveil Gun Control Reforms</a>
+			<a class="menu_base" href="#">Federal Prosecutors Take Softer Approach To Corporate Crime</a>
+			<a class="menu_base" href="#">Montana Risks Losing Federal Education Funding</a>
 		</div>
 	</span>
 	<span>
 		<div id="colonne_5" class="index_col">
-			<a href="#">Conservative Kingmaker Urges GOP Candidates To Sign Marriage Vow</a>
-			<a href="#">House Dems Scramble After Obama Insists On Grand Bargain</a>
-			<a href="#">Congressional Black Caucus Members Criticize Obama On Unemployment</a>
-			<a href="#">New Twist In Casey Anthony Release Date</a>
-			<a href="#">Syria Accuses U.S. Of Inciting Unrest</a>
-			<a href="#">Kate Makes Girl's Princess Dream Come True</a>
-			<a href="#">David Laws' Swift Return To Parliament</a>
-			<a href="#">They've Come To Say Goodbye To Their Childhoods</a>
+			<a class="menu_base" href="#">Conservative Kingmaker Urges GOP Candidates To Sign Marriage Vow</a>
+			<a class="menu_base" href="#">House Dems Scramble After Obama Insists On Grand Bargain</a>
+			<a class="menu_base" href="#">Congressional Black Caucus Members Criticize Obama On Unemployment</a>
+			<a class="menu_base" href="#">New Twist In Casey Anthony Release Date</a>
+			<a class="menu_base" href="#">Syria Accuses U.S. Of Inciting Unrest</a>
+			<a class="menu_base" href="#">Kate Makes Girl's Princess Dream Come True</a>
+			<a class="menu_base" href="#">David Laws' Swift Return To Parliament</a>
+			<a class="menu_base" href="#">They've Come To Say Goodbye To Their Childhoods</a>
 		</div>
 	</span>
 	
 	<div id="menu_navigation">
-		<a href="#">&#8592;</a><a href="#">&#8594;</a>
+		<a class="menu_base" href="#">&#8592;</a><a class="menu_base" href="#">&#8594;</a>
 	</div>
 </div><!-- menu_index  -->
 
