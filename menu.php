a266db374caefbba4c06c3b52acac60ca8c160bb
Sebastien Sanfilippo <sebastien@sebastien-MacBookPro.(none)>
2011-07-14 15:37:01 +0200

    menu-update2

--- menu.php
+++ menu.php
@@ -124,7 +124,7 @@ var theCity = "<?php echo $_GET["city"] ?>";
 	</span>
 	
 	<div id="menu_navigation">
-		<a class="menu_base" href="#">&#8592;</a><a class="menu_base" href="#">&#8594;</a>
+	<a class="arrows" href="#">&#8592;</a> <a class="arrows" href="#">&#8594;</a>
 	</div>
 </div><!-- menu_index  -->
 
@@ -135,6 +135,8 @@ var theCity = "<?php echo $_GET["city"] ?>";
 	<span><a href="#" target = "blank">Institutions</a></span>
 	<span><a href="#" target = "blank">Writings</a></span>
 	<span><a href="#" target = "blank">Events</a></span>
+	<span><a href="#" target = "blank">About us</a></span>
+
 </div>
 
 
