990d7b571d8cf7758ab7ac20f1c56070c8edd8f2
Pierre Marchand <pierre@deb-macist.(none)>
2011-07-26 15:30:36 +0200

    wordpressified menu.php

--- menu.php
+++ menu.php
@@ -1,219 +1,23 @@
 <?php
-/**
-
-<<<<<<< HEAD
-Sounds of Europe
-=======
-<html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
-<title>SOE - PROJECT</title>
-<style media="screen" type="text/css">
-@import url("style.css");
-@import url("menu_top.css");
-@import url("map.css");
-@import url("menu_map.css");
-@import url("menu_nomap.css");
-@import url("content.css");
-</style>
-
-
-<script type='text/javascript' src='JSON-js/json_parse.js'></script>
-<script type='text/javascript' src='js/jquery.js'></script>
-<script type='text/javascript' src='js/raphael.js'></script>
-<script type='text/javascript' src='js/soe.js'></script>
-
-<script type='text/javascript'>
-var theCity = "<?php echo $_GET["city"] ?>";
-</script>
-
-</head>
-<body>
-
-<div id="carte">
-
-</div>
-
-<div id="content_outer">
-<div id="content">
-<div class="content_category">ARTISTS</div>
-<div class="title">Jan Vandam</div>
-<div class="location">Brussels - Belgium</div>
-<div class="picture"><img src="img/BeritGreinke_SHHH_11.jpg" width="80%"></div>
-<div class="section">
-<div class="section_title">Biography</div>
-<div class="section_par">
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
-</div>
-<div class="section_title">Use of fieldrecordings</div>
-<div class="section_par">
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</div>
-</div>
-</div>
-<div class="general_url">
-<a href="#">www.janvandam.com</a>
-</div>
-<div class="tags_box">
-<div class="tags">
-<a href="#">tag 1</a>, <a href="#">tag 2</a>, <a href="#">tag 3</a>
-</div>
-</div>
-
-<div class="artist_track"> Listen to <img src="img/arrow_sound.png"/> Title of Sound </div>
-</div> <!-- content -->
-</div> <!-- content_outer  -->
-
-
-
-<!-- located objects -->
-<div id="located_type">
-	<div class="located_type_item type_artist"><span>ARTISTS</span></div>
-	<div class="located_type_item type_event"><span>EVENTS</span></div>
-	<div class="located_type_item type_organization"><span>INSTITUTIONS</span></div>
-</div><!-- located_type -->
-
-<div id="located_object_artist" class="located_object">
-
-	<div id="closedBox_outer">
-		<div id="closedBox">
-			<div class="closedBox_category">ARTISTS</div>
-			<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-		</div> 
-	</div><!-- fin -->
-
-	<div id="closedBox_outer">
-		<div id="closedBox">
-			<div class="closedBox_category">ARTISTS</div>
-			<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-		</div> 
-	</div><!-- fin -->
-
-	<div id="closedBox_outer">
-		<div id="closedBox">
-			<div class="closedBox_category">ARTISTS</div>
-			<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-		</div> 
-	</div><!-- fin -->
-
-	<div id="closedBox_outer">
-		<div id="closedBox">
-			<div class="closedBox_category">ARTISTS</div>
-			<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-		</div> 
-	</div><!-- fin -->
-
-	<div id="closedBox_outer">
-		<div id="closedBox">
-			<div class="closedBox_category">ARTISTS</div>
-			<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-		</div> 
-	</div><!-- fin -->
-
-</div><!-- located_object -->
-
-<div id="located_object_event" class="located_object">
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">EVENTS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">EVENTS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">EVENTS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">EVENTS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">EVENTS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-</div><!-- located_object -->
-
-
-<div id="located_object_organization" class="located_object">
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">ORGANIZATIONS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">ORGANIZATIONS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">ORGANIZATIONS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">ORGANIZATIONS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-<div id="closedBox_outer">
-<div id="closedBox">
-<div class="closedBox_category">ORGANIZATIONS</div>
-<div class="closedBox_title"><a href="#">Jan Vandam</a></div>
-</div> 
-</div><!-- fin -->
-
-</div><!-- located_object -->
->>>>>>> ce6b3cb360fe04091bfb4cefec9bf0655b64ba5c
-
-%FILE%		menu.php
-%AUTHOR%	Pierre Marchand
-%DATE%		2011-07-25
-
-*/
+/** 
+ Sounds of Europe
+ 
+ %FILE%		menu.php
+ %AUTHOR%	Pierre Marchand
+ %DATE%		2011-07-25
+ 
+ */
 
 ?>
 
 <div id="menu_index" class="menu_closed">
 
-<<<<<<< HEAD
 </div> <!--menu_index-->
-=======
-<div id="menu_navigation">
-<a class="arrows" href="#">&#8592;</a><a class="arrows" href="#">&#8594;</a>
-</div>
-</div><!-- menu_index  -->
->>>>>>> ce6b3cb360fe04091bfb4cefec9bf0655b64ba5c
 
 <div id="menu_item">
-	<span><a href="#">Blog</a></span>
-	<span><a href="#">Artists</a></span>
-	<span><a href="#">Events</a></span>
-	<span><a href="#">Institutions</a></span>
-	<span><a href="#">Writings</a></span>
+<span><a href="#">Blog</a></span>
+<span><a href="#">Artists</a></span>
+<span><a href="#">Events</a></span>
+<span><a href="#">Institutions</a></span>
+<span><a href="#">Writings</a></span>
 </div>
