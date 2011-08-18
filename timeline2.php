745d80e6294176e5a322ad583c1be1b298e8f965
Ludivine Loiseau <ludi.loiseau@gmail.com>
2011-08-18 15:57:05 +0200

    essais tableaux timeline

---
+++ timeline2.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
+<title>SOE - PROJECT</title>
+<style media="screen" type="text/css">
+@import url("style.css");
+@import url("map.css");
+@import url("menu_map.css");
+@import url("menu_nomap.css");
+@import url("content.css");
+@import url("writings.css");
+</style>
+
+<script type='text/javascript' src='JSON-js/json_parse.js'></script>
+<script type='text/javascript' src='js/jquery.js'></script>
+<script type='text/javascript' src='js/raphael.js'></script>
+<script type='text/javascript' src='js/soe.js'></script>
+
+<script type='text/javascript'>
+var theCity = "<?php echo $_GET["city"] ?>";
+</script>
+
+</head>
+<body>
+
+
+
+ <div id="menu_index" class="menu_closed">
+	  <table class="sample" >
+		  <tr class="month_row">
+			  <td background=img/vignette-1.png class="month_column">October</td>
+			  <td background=img/vignette-1.png class="month_column">November</td>
+			  <td background=img/vignette-1.png class="month_column">December</td>
+		  </tr>
+		  <tr class="workshop_row">
+			  <td background=img/vignette-2.png class="workshop_column">Workshop + Residencies</td>
+		  </tr>
+		  <tr class="webproject_row">
+			  <td background=img/vignette-3.png class="webproject_column">Webproject around europe</td>
+		  </tr>
+		  <tr class="cities_row">
+			  <td background=img/vignette-4.png class="cities_column">Brussels</td>
+		  </tr>
+		  <tr class="event_name_row">
+			  <td background=img/vignette-5.png class="event_name_column">Festival Opening</td>
+		  </tr>
+		  
+	  </table> 	
+  </div><!-- menu_index  -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
