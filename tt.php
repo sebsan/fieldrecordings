1044734ed89a1bb377b3fc9cf5146b459c10ea93
Sebastien Sanfilippo <sebastien@sebastien-MacBookPro.(none)>
2011-09-16 14:31:25 +0200

    tt

---
+++ tt.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en"> 
+<head> 
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Sounds of Europe</title>
+
+<style media="screen" type="text/css">
+@import url("http://localhost/~sebastien/fieldrecordings/style.css");
+@import url("http://localhost/~sebastien/fieldrecordings/menu_top.css");
+@import url("http://localhost/~sebastien/fieldrecordings/map.css");
+@import url("http://localhost/~sebastien/fieldrecordings/menu_map.css");
+@import url("http://localhost/~sebastien/fieldrecordings/menu_nomap.css");
+@import url("http://localhost/~sebastien/fieldrecordings/content.css");
+</style>
+<body>
+<?php readfile("timeline2.html"); ?>
+</body>
+
