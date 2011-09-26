f4c520d034e2105188dc2dee6953f01211cc74f2
Sebastien Sanfilippo <sebastien@sebastien-MacBookPro.(none)>
2011-09-26 18:29:15 +0200

    wave

---
+++ brochure/wave.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+
+import sys
+try:
+	import scribus
+except ImportError:
+	print "This script only works from within Scribus"
+	sys.exit(1)
+
+obj = scribus.getSelectedObject(0)
+
+sdir = 1
+# options
+fs = 8
+fsmax = 14
+fsmin = 8
+inc = 0.005
+###
+
+tl = scribus.getTextLength()
+for c in range(tl - 1):
+	scribus.selectText(c, 1, obj)
+	if(sdir == 1):
+		fs += inc
+		if(fs > fsmax):
+			sdir = 0
+	else:
+		fs -= inc
+		if(fs < fsmin):
+			sdir = 1
+	scribus.setFontSize(fs,obj)
