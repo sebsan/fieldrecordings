b607d2b4702d89f73a59d9f7437d351cd612cf9a
Sebastien Sanfilippo <sebastien@sebastien-MacBookPro.(none)>
2011-10-28 15:08:03 +0200

    tout

--- menu.php
+++ menu.php
@@ -75,7 +75,7 @@ if(/*$isEntryPoint === */true)
 
 <div id="bando">
 	<div id="bando_logo">
-		<a href="<?php echo get_bloginfo('wpurl'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/soe-logo-menu.png"/></a>
+		<a href="<?php echo get_bloginfo('wpurl'); ?>"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/soe-logo-menu2.png"/></a>
 		<a href="http://ec.europa.eu/culture/our-programmes-and-actions/doc411_en.htm"><img src="<?php echo get_stylesheet_directory_uri(); ?>/img/eu-logo.png"/></a>
 	</div>
 
