a7a91c259153d6a659f383520715a38e71cc59d1
Sebastien Sanfilippo <sebastien@sebastien-MacBookPro.(none)>
2011-11-17 14:37:28 +0100

    changements lien menu organisation

--- soe_organisation.php
+++ soe_organisation.php
@@ -120,7 +120,7 @@ $regBlock = '
 <div class="menu_category">Sign up</div>
 <span class="menu_base_2"><p>If you want to sign up your organisation, 
 you can send us an email with a short description of your organisation 
-and website at <a class="menu_base" href="mailto:info@soundsofeurope.eu">info@soundsofeurope.eu</a></p></span>
+and website at <a class="menu_base_2"href="mailto:info@soundsofeurope.eu">info@soundsofeurope.eu</a></p></span>
 </div>
 ';
 
