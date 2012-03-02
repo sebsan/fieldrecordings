63b6045c47c523e307d3292dd7aa3b3ed67f1426
Pierre Marchand <pierre@macist2.(none)>
2012-03-02 11:16:57 +0100

    xhtml*

--- header.php
+++ header.php
@@ -41,6 +41,9 @@ $SOE_styles = array(
 	"menu_nomap",
 	"content",
 	"writings");
+	
+if(true)
+    echo '<?xml version="1.0"?>'."\n";
 
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en"> 
