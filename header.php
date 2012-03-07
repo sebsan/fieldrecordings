288088c7688c8d251c265d122d645c44bd018e4b
Pierre Marchand <pierre@macist2.(none)>
2012-03-07 12:43:14 +0100

    give a try to html5

--- header.php
+++ header.php
@@ -42,11 +42,11 @@ $SOE_styles = array(
 	"content",
 	"writings");
 	
-if(true)
+if(false)
     echo '<?xml version="1.0"?>'."\n";
 
-?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"> 
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en"> 
+?><!DOCTYPE html> 
+<html> 
 <head> 
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title><?php
