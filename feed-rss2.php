b104b187ab91e29aa248c41f17c19b60835cdca1
Pierre Marchand <pierre@oep-h.com>
2012-06-05 09:58:05 +0200

    RSS2 feed, remove comments

--- feed-rss2.php
+++ feed-rss2.php
@@ -2,7 +2,6 @@
 /**
  * RSS2 Feed Template for displaying RSS2 Posts feed.
  *
- * @package WordPress
  */
 
 header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
@@ -38,9 +37,7 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 	<item>
 		<title><?php the_title_rss() ?></title>
 		<link><?php the_permalink_rss() ?></link>
-		<comments><?php comments_link_feed(); ?></comments>
 		<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
-		<dc:creator><?php the_author() ?></dc:creator>
 		<?php the_category_rss('rss2') ?>
 
 		<guid isPermaLink="false"><?php the_guid(); ?></guid>
